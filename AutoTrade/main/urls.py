d7b523a7322239d76b9476655704d846559eaa5f
sujin-create <qsoojinq@naver.com>
2022-04-09 16:11:24 +0900

    Fix : modify navbar url

--- AutoTrade/main/urls.py
+++ AutoTrade/main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path,include
 import main.views as views
 
+
+app_name = 'Home'
+
 urlpatterns = [
     path('',views.index,name='main'),
     path('detail',views.detail,name='detail')
