ccb95d4b9c875d6deed54417b4f8d4654182ee78
sujin-create <qsoojinq@naver.com>
2022-05-26 03:02:58 +0900

    Fix(database) : sentiment관련 table수정 및 추가

--- AutoTrade/Sentiment/models.py
+++ AutoTrade/Sentiment/models.py
@@ -5,8 +5,9 @@ class Sentiment(models.Model):
     crawling_date = models.DateField(primary_key=True)
     n_count = models.SmallIntegerField()
     p_count = models.SmallIntegerField()
-    neu_count = models.SmallIntegerField()
+    e_count = models.SmallIntegerField()
     k_val = models.FloatField()
+    com_count = models.IntegerField()
 
     class Meta:
         managed = False
@@ -16,8 +17,18 @@ class Sentiment(models.Model):
 class Comments(models.Model):
     crawling_date = models.ForeignKey('Sentiment', models.DO_NOTHING, db_column='crawling_date')
     comment = models.TextField()
-    sentiment = models.CharField(max_length=8)
+    semtiment = models.CharField(max_length=8)
+    weight = models.FloatField()
 
     class Meta:
         managed = False
         db_table = 'comments'
+
+class Topten(models.Model):
+    crawling_date = models.ForeignKey(Sentiment, models.DO_NOTHING, db_column='crawling_date')
+    word = models.CharField(max_length=50)
+    count = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'topten'
