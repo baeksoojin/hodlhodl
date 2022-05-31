d6a9e4a2773dea20d5c32633077d8b53d90d246a
sujin-create <qsoojinq@naver.com>
2022-05-31 19:12:03 +0900

    merge sentiment-analysis

--- AutoTrade/Sentiment/models.py
+++ AutoTrade/Sentiment/models.py
@@ -5,8 +5,9 @@ class Sentiment(models.Model):
     crawling_date = models.DateField(primary_key=True)
     n_count = models.SmallIntegerField()
     p_count = models.SmallIntegerField()
-    neu_count = models.SmallIntegerField()
+    e_count = models.SmallIntegerField()
     k_val = models.FloatField()
+    com_count = models.IntegerField()
 
     class Meta:
         managed = False
@@ -17,7 +18,18 @@ class Comments(models.Model):
     crawling_date = models.ForeignKey('Sentiment', models.DO_NOTHING, db_column='crawling_date')
     comment = models.TextField()
     sentiment = models.CharField(max_length=8)
+    weight = models.FloatField()
+    vote = models.IntegerField()
 
     class Meta:
         managed = False
         db_table = 'comments'
+
+class WordcloudTable(models.Model):
+    crawling_date = models.ForeignKey(Sentiment, models.DO_NOTHING, db_column='crawling_date')
+    word = models.CharField(max_length=50)
+    count = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'wordcloud_table'
