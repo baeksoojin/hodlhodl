7a45fb8ade5fa8271065dfe6dacd06122fd838a0
sujin-create <qsoojinq@naver.com>
2022-06-10 00:47:45 +0900

    Fix : merge conflict

--- AutoTrade/Sentiment/views.py
+++ AutoTrade/Sentiment/views.py
@@ -20,32 +20,28 @@ def sentiment_result(request):
     # res_data=json.dumps(res_data)
 
     # 따라서, 예제코드로 작성해서 test를 진행한다.
-    sentiment = Sentiment
+    # sentiment = Sentiment
     # sentiment(crawling_date = "2022-05-25", n_count = 30, p_count = 50, e_count = 60, k_val = 0.478, com_count = 140).save()
 
-    sentiment = sentiment.objects.first()
-    res_data = model_to_dict(sentiment)
-    print(res_data)
-    res_data['crawling_date'] = res_data['crawling_date'].strftime("%Y %b %d")
-    res_data = json.dumps(res_data)
+    # sentiment = sentiment.objects.first()
+    # res_data = model_to_dict(sentiment)
+    # print(res_data)
+    # res_data['crawling_date'] = res_data['crawling_date'].strftime("%Y %b %d")
+    # res_data = json.dumps(res_data)
     
-    # 서버에서 크롤링 이후에 token화해서 단어들을 나열하고 for문을 돌려서 가장 많이 나온 단어 10개를 추출한다.
     # 각 단어의 언급량을 weight로 설정하여 워드 클라우딩을 진행한다.
 
-    # 10개의 결과를 모델에 임의로 저장하고 그것을 다시 불러와서 사용하는 예제코드를 작성
+    wordcloud = WordcloudTable.objects.last() #가장 최근의 키워드 추출 결과를 출력
+    print(wordcloud)
+    temp = model_to_dict(wordcloud)
+    print(temp)
+    date = temp['crawling_date']
+    print(date)
 
-    word = ["like","good","hit","buy","dead","love","fall","excellent","invest","get"]
-    count = [50,40,30,10,9,8,7,6,5,4]
-    #10개의 데이터를 임의로 설정하여 저장
-    # for i in range(0,10):
-    #     wordcloud = WordcloudTable()
-    #     wordcloud.crawling_date = Sentiment.objects.get(crawling_date = "2022-05-25")
-    #     wordcloud.word = word[i]
-    #     wordcloud.count = count[i]
-    #     wordcloud.save()
-
-    wordcloud = WordcloudTable.objects.all()
+    wordcloud = WordcloudTable.objects.filter(crawling_date = date)
+    print(wordcloud)
+    
     res_data2=serializers.serialize("json",wordcloud)
 
 
-    return render(request,"AI_result/sentiment.html",{'res_data' : res_data, 'res_data2' : res_data2})
+    return render(request,"AI_result/sentiment.html",{ 'res_data2' : res_data2})
