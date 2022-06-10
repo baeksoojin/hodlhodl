91e14608ef43310276d11cd8a352f67a687ba309
sujin-create <qsoojinq@naver.com>
2022-06-11 04:42:24 +0900

    ADD : 추가 이상감탐지 결과 제공

--- AutoTrade/CoinTrading/models.py
+++ AutoTrade/CoinTrading/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Detect(models.Model):
+    prediction_date = models.DateTimeField()
+    close_chg = models.FloatField()
+    sell = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'detect'
+
+class Result(models.Model):
+    result_date = models.DateField(primary_key=True)
+    hpr_percent = models.FloatField()
+    mdd = models.FloatField(db_column='MDD')  # Field name made lowercase.
+    buy_hold_hpr_percent = models.FloatField()
+    buy_hold_mdd = models.FloatField(db_column='buy_hold_MDD')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'result'
